a03b9b33479ea9cbb52d258f8c853dea12d5d785
miniece <mvelinda@icloud.com>
2022-10-31 22:21:05 -0400

    great value version of reddit

---
+++ PaupersReddit/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaupersReddit.Models;
+using System.Diagnostics;
+
+namespace PaupersReddit.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        RDAL api = new RDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PostView()
+        {
+            SWPost sp = api.GetPost();
+
+            return View(sp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PaupersReddit/Models/RDAL.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+
+namespace PaupersReddit.Models
+{
+    public class RDAL
+    {
+        public SWPost GetPost()
+        {
+            var client = new RestClient($"https://www.reddit.com/r/aww/.json");
+            var request = new RestRequest();
+            var response = client.GetAsync<SWPost>(request);
+            SWPost d = response.Result;
+            return d;
+
+        }
+    }
+}
